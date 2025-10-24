afc29d39beceaf36810311ac49bc2830477d3ac8
soniarosem <soniamohammedb2b@gmail.com>
2025-10-24 01:34:36 -0400

    Added get streets route

---
+++ App/views/street.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, jsonify
+from App.controllers.street import get_all_streets_json
+
+street_views = Blueprint('street_views', __name__, template_folder='../templates')
+
+@street_views.route('/api/streets', methods=['GET'])
+def get_streets_action():
+    streets = get_all_streets_json()
+    return jsonify({'data': streets}), 200
